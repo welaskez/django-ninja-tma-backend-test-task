03b644fe9aa06bedbe3ae235bd513cb5a0e3286c
welaskez <nazar.nikitenko.03@mail.ru>
2025-02-22 21:12:16 +0500

    fix: check & minus user balance

--- api/upgrades/api.py
+++ api/upgrades/api.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from ninja.errors import HttpError
 from ninja.router import Router
 from users.models import User
 
@@ -17,6 +18,12 @@ def get_upgrades(request):
 def buy_upgrade(request, upgrade_id: int):
     upgrade = get_object_or_404(Upgrade, id=upgrade_id)
     user = get_object_or_404(User, id=request.auth.id)
+
+    if user.balance >= upgrade.price:
+        user.balance -= upgrade.price
+    else:
+        raise HttpError(status_code=400, message="Not enough balance")
+
     user.upgrades.add(upgrade)
     user.save()
 

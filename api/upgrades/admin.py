2cfc9384ca26ac571c3df6fa80d01170dda5742f
welaskez <nazar.nikitenko.03@mail.ru>
2025-02-22 20:16:32 +0500

    feat: create upgrade model

--- api/upgrades/admin.py
+++ api/upgrades/admin.py
@@ -1,1 +1,9 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Upgrade
+
+
+@admin.register(Upgrade)
+class UpgradeModel(admin.ModelAdmin):
+    list_display = ["name", "photo", "price", "boost"]
+    list_editable = ["photo"]
--- api/upgrades/models.py
+++ api/upgrades/models.py
@@ -1,1 +1,21 @@
-# Create your models here.
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Upgrade(models.Model):
+    name = models.CharField(verbose_name=_("Upgrade name"), max_length=150)
+    photo = models.URLField(verbose_name=_("Upgrade photo"))
+    price = models.DecimalField(
+        verbose_name=_("Upgrade price"),
+        max_digits=10,
+        decimal_places=2,
+    )
+    boost = models.DecimalField(
+        verbose_name=_("Balance per hour boost"),
+        max_digits=10,
+        decimal_places=2,
+        default=1.0,
+    )
+
+    def __str__(self):
+        return self.name

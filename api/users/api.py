eca63d00580b8a4fd49435ea04632ee31f222b42
welaskez <nazar.nikitenko.03@mail.ru>
2025-02-23 15:23:26 +0500

    feat: added update balance endpoint

--- api/users/api.py
+++ api/users/api.py
@@ -1,3 +1,4 @@
+from ninja.errors import HttpError
 from ninja.router import Router
 
 from users.schemas import UserRead
@@ -9,4 +10,15 @@ router = Router()
 def get_user(request):
     user = request.auth
     user.update_balance()
+    return user
+
+
+@router.post(path="/update_balance", response=UserRead)
+def update_user_balance(request, balance: int):
+    user = request.auth
+    if balance > 0:
+        user.balance += balance
+    else:
+        raise HttpError(status_code=400, message="Invalid balance")
+    user.save()
     return user

dd6fa64937360009427fb4e22794245e63816840
welaskez <nazar.nikitenko.03@mail.ru>
2025-02-23 17:05:03 +0500

    feat: create task model

--- api/tasks/admin.py
+++ api/tasks/admin.py
@@ -1,1 +1,8 @@
-# Register your models here.
+from django.contrib import admin
+
+from tasks.models import Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    pass
--- api/tasks/models.py
+++ api/tasks/models.py
@@ -1,1 +1,16 @@
-# Create your models here.
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Task(models.Model):
+    class TaskType(models.TextChoices):
+        TELEGRAM = "telegram"
+        X = "x"
+        INSTAGRAM = "instagram"
+        YOUTUBE = "youtube"
+
+    photo = models.URLField(verbose_name=_("Task photo"))
+    name = models.CharField(verbose_name=_("Task name"), max_length=150)
+    link = models.URLField(verbose_name=_("Task link"))
+    reward = models.DecimalField(verbose_name=_("Task reward"), max_digits=10, decimal_places=2)
+    type = models.CharField(verbose_name=_("Task type"), max_length=32, choices=TaskType)
